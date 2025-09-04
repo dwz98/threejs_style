de2bf8697aee6ebe6a1367c76e4b76d162d2d511
邓文志 <d1110xcx@163.com>
2025-09-04 14:23:16 +0800

    删除所有

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import Router from "@/router/Router";
-
-export default function App() {
-	return <Router />;
-}
--- src/utils/three/scene.ts
+++
@@ -1,7 +0,0 @@
-import * as THREE from "three";
-
-export const createScene = (backgroundColor: string = "#222333") => {
-	const scene = new THREE.Scene();
-	scene.background = new THREE.Color(backgroundColor);
-	return scene;
-};
